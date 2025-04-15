59572b15a7dbd4c01931342489494afd8f52debe
Gilberto <gil@xatrix.mx>
2025-04-15 17:49:01 -0600

    Implementacion de funciones victoria e impresion de tablero

--- main.py
+++ main.py
@@ -1,5 +1,85 @@
 from random import choice
 
+#Funcion para imprimir el tablero de forma estetica
+def imprimir_tablero():
+    for fila in board:
+        print('+-------+-------+-------+')
+        print('|       |       |       |')
+        print('|', end='')
+        for celda in fila:
+            print(f'   {celda}   |', end='')
+        print()
+        print('|       |       |       |')
+    print('+-------+-------+-------+')
+
+
+#Funcion para encontrar el ganador
+def victoria():
+    #Filas
+    if board[0][0] == board[0][1] == board[0][2] and board[0][0] in ['X', 'O']:
+        if board[0][0] == 'X':
+            print('¡La computadora ha ganado!')
+        else:
+            print('¡Has ganado!')
+        return True
+
+    if board[1][0] == board[1][1] == board[1][2] and board[1][0] in ['X', 'O']:
+        if board[1][0] == 'X':
+            print('¡La computadora ha ganado!')
+        else:
+            print('¡Has ganado!')
+        return True
+
+
+    if board[2][0] == board[2][1] == board[2][2] and board[2][0] in ['X', 'O']:
+        if board[2][0] == 'X':
+            print('¡La computadora ha ganado!')
+        else:
+            print('¡Has ganado!')
+        return True
+
+
+    #Columnas
+    if board[0][0] == board[1][0] == board[2][0] and board[0][0] in ['X', 'O']:
+        if board[0][0] == 'X':
+            print('¡La computadora ha ganado!')
+        else:
+            print('¡Has ganado!')
+        return True
+    
+    if board[0][1] == board[1][1] == board[2][1] and board[0][1] in ['X', 'O']:
+        if board[0][1] == 'X':
+            print('¡La computadora ha ganado!')
+        else:
+            print('¡Has ganado!')
+        return True
+
+    if board[0][2] == board[1][2] == board[2][2] and board[0][2] in ['X', 'O']:
+        if board[0][2] == 'X':
+            print('¡La computadora ha ganado!')
+        else:
+            print('¡Has ganado!')
+        return True
+
+
+    #Diagonales
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] in ['X', 'O']:
+        if board[0][0] == 'X':
+            print('¡La computadora ha ganado!')
+        else:
+            print('¡Has ganado!')
+        return True
+
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] in ['X', 'O']:
+        if board[0][2] == 'X':
+            print('¡La computadora ha ganado!')
+        else:
+            print('¡Has ganado!')
+        return True
+    
+    return False
+
+
 #Lista completa del 1 al 9
 flat_board = [i for i in range(1, 10)]
 board = []
@@ -14,21 +94,10 @@ for i in range(0, 9, 3):
 num = 5
 fila = (num - 1) // 3
 columna = (num - 1) % 3
-
 board[fila][columna] = 'X'
 
-
-#Imprimimos el tablero en orden y centrado
-for fila in board:
-    print('+-------+-------+-------+')
-    print('|       |       |       |')
-    print('|', end='')
-    for celda in fila:
-        print(f'   {celda}   |', end='')
-    print()
-    print('|       |       |       |')
-print('+-------+-------+-------+')
-
+#Imprimimos el tablero
+imprimir_tablero()
 
 
 while True:
@@ -55,18 +124,12 @@ while True:
 
     board[fila][columna] = 'O'
 
-    #Imprimimos el tablero en orden y centrado
-    for fila in board:
-        print('+-------+-------+-------+')
-        print('|       |       |       |')
-        print('|', end='')
-        for celda in fila:
-            print(f'   {celda}   |', end='')
-        print()
-        print('|       |       |       |')
-    print('+-------+-------+-------+')
+    #Imprimimos el tablero
+    imprimir_tablero()
 
-
+    #Verificamos ganador
+    if victoria():
+        break
 
     #Guardar numeros disponibles
     num_disp = []
@@ -80,52 +143,11 @@ while True:
     num = choice(num_disp)
     fila = (num - 1) // 3
     columna = (num - 1) % 3
-
     board[fila][columna] = 'X'
 
-    #Imprimimos el tablero en orden y centrado
-    for fila in board:
-        print('+-------+-------+-------+')
-        print('|       |       |       |')
-        print('|', end='')
-        for celda in fila:
-            print(f'   {celda}   |', end='')
-        print()
-        print('|       |       |       |')
-    print('+-------+-------+-------+')
+    #Imprimimos el tablero
+    imprimir_tablero()
 
-    #break
-
-
-def victoria():
-    #Filas
-    if board[0][0] == board[0][1] == board[0][2] and board[0][0] in ['X', 'O']:
-        print('Hay un ganador!!!')
-
-    elif board[1][0] == board[1][1] == board[1][2] and board[1][0] in ['X', 'O']:
-        print('Hay un ganador!!!')
-
-    elif board[2][0] == board[2][1] == board[2][2] and board[2][0] in ['X', 'O']:
-        print('Hay un ganador!!!')
-
-
-    #Columnas
-    if board[0][0] == board[1][0] == board[2][0] and board[0][0] in ['X', 'O']:
-        print('Hay un ganador!!!')
-    
-    elif board[0][1] == board[1][1] == board[2][1] and board[0][1] in ['X', 'O']:
-        print('Hay un ganador!!!')
-
-    elif board[0][2] == board[1][2] == board[2][2] and board[0][2] in ['X', 'O']:
-        print('Hay un ganador!!!')
-
-
-    #Diagonales
-    if board[0][0] == board[0][1] == board[0][2] and board[0][0] in ['X', 'O']:
-        print('Hay un ganador!!!')
-
-    elif board[0][0] == board[0][1] == board[0][2] and board[0][0] in ['X', 'O']:
-        print('Hay un ganador!!!')
-
-    elif board[0][0] == board[0][1] == board[0][2] and board[0][0] in ['X', 'O']:
-        print('Hay un ganador!!!')
+    #Verificamos ganador
+    if victoria():
+        break
